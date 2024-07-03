7add681d9233e90b21aff8491ab16bf6d4a3ee0c
ilarramendi <ilarramendi1@gmail.com>
2024-07-02 23:24:19 -0300

    changes to wait time

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ async function start() {
 		return;
 	}
 	const requests = jobs.map(j => j.requests.length).sort((a, b) => b - a)[0];
-	const tryingIn = Math.min(1000 * (requests * 10 + 60), 60000); // 10 seconds per request + 60 base, max 60 minutes
-	console.log(`${jobs.length} jobs still pending, with ${requests} requests, checking batch status: ${(tryingIn / 60000).toFixed(1)}m`);
+	const tryingIn = Math.min(1000 * (requests * 10 + 60), 3_600_000); // 60s + 10s x request, max 1h
+	console.log(`${jobs.length} jobs still pending, with ${requests} requests, checking batch status: ${(tryingIn / 60_000).toFixed(tryingIn > 600_000 ? 0 : 1)}m`);
 	await new Promise(resolve => setTimeout(resolve, tryingIn));
 	return start();
 }
